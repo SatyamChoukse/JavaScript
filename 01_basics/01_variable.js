54c9d91e9d94d6ee87422d7eafd0e22de9980f80
SatyamChoukse <122443413+SatyamChoukse@users.noreply.github.com>
2023-12-27 20:16:52 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 206201158
+let accountEmail = "satyam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jabalpur"
+let accountState;
+
+// accountId = 2  //can't be change giving error;
+
+accountEmail ="sa@gmail.com"
+accountPassword = "112233"
+accountCity = "satna"
+
+console.log(accountEmail); 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"  //treat all JS code as new version
+
+// alert( 3 + 4 ) //we are using nodejs, not a browser
+
+
+// Number => 2 to the power of 51
+// string => ""
+//boolean => true/false
+// null =>stand alone value
+// undefined 
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
