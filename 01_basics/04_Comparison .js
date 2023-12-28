6316a559dd1422bd1b6f5d984b50737731959cc1
SatyamChoukse <122443413+SatyamChoukse@users.noreply.github.com>
2023-12-28 07:01:21 +0000

    comparison

---
+++ 01_basics/04_Comparison .js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log("2" == 2); //true - b/c it compare value only
+
+console.log("2" === 2); //false - b/c it compare value with its datatype
