3db8cf6e4ef49ea1bb327e4241154e5aa18f6f57
SatyamChoukse <122443413+SatyamChoukse@users.noreply.github.com>
2023-12-28 07:24:58 +0000

    datatype brief summery

---
+++ datatype_whole_summery.js
@@ -0,0 +1,49 @@
+// Datatype is divided into two categorys 1)primitive 2)Non primitive
+
+//  PRIMITIVE -> 
+
+//  7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+const value= 100
+const val = 100.3
+
+const isLoggedIn= false
+const outsideTemp = null
+let useremail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+console.log(id);
+console.log(anotherId);
+
+
+// Reference(Nonprimitive)
+
+// Array, Objects, Functions
+
+const Heros = ["Shaktimaan", "Naagraj","Doga"];   //Array
+
+let myObj = {                                     //Object - always in key value pair
+    name:"Satyam",
+    age:21,
+}
+
+const myFun = function(){                           //Functions
+    console.log("Hello World");
+}
+
+
+// Type of val	Result
+
+// Undefined	"undefined"
+// Null	        "object"
+// Boolean	    "boolean"
+// Number	    "number"
+// String	    "string"
+// Function     "function"
+// Object (native and does not implement [[Call]])	    "object"
+// Object (native or host and does implement [[Call]])	"function"
+// Object (host and does not implement [[Call]])	Implementation-defined except may not be "undefined", "boolean", "number", or "string".
