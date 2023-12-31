a3502c1ff5810db86bb8781129e54b0500b5d80d
SatyamChoukse <122443413+SatyamChoukse@users.noreply.github.com>
2023-12-31 18:57:01 +0000

    Objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,39 @@
+// Declaration of objects
+
+// 1) Singleton
+// Object.create
+
+// 2) object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+    name: "Satyam",
+    "fullname" : "Chouksey",
+    age: 21,
+    [mySym]: "myKey1",
+    location: "Jabalpur",
+    email: "satyam@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"]);
+// console.log(JsUser["fullname"]);
+
+console.log(JsUser[mySym]);
+console.log(typeof JsUser[mySym]);
+
+// agar object mai kisi value ko change karna ho to wo "over-write" ho jati hai **
+JsUser.email = "satychouksey@gmail.com"
+
+console.log(JsUser["email"]);
+
+// Agar hume object mai koi change na kar sake to uske liye hume usse freeze karna padta hai
+
+Object.freeze(JsUser)
+
+JsUser.email = "satyhjkshajkhchouksey@gmail.com"
+
+console.log(JsUser);
